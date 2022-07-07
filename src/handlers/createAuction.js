3f25c0a8cd6e7588cd7e293ffcd30704fc0aa2a2
Olivia Zhang <oliviaz@apple.com>
2022-07-06 17:04:09 -0700

    Wed Jul  6 17:04:09 PDT 2022

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,18 @@
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  // need to be store in db not memory as we are not guarantee hitting the specific instance
+  const auction = {
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    body: JSON.stringify({ auction }),
   };
 }
 
 export const handler = createAuction;
-
-
